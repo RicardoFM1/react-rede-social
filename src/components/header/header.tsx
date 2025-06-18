7b2adf03cf7d7954a85ebabad1148183c781d536
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-06-17 21:55:58 -0300

    css mudado

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,13 +7,12 @@ return <header className={style.header}>
 <h1 className={style.Titulo}>The Script</h1>
 <Iconify icon="lets-icons:search-light" ClassName={style.IconPesquisa}/>
 <input className={style.Pesquisa} type="text" placeholder="Pesquise um perfil..."/>
-
 </div>
-<div className="botoesHeader">
-<button>Criar</button>
-<button>Perfil</button>
-<button>Mensagens</button>
-<button>Configurações</button>
+<div className={style.botoesHeader}>
+<button><Iconify icon="fa-solid:plus" color="White"/></button>
+<button className={style.btnUser}><Iconify icon="heroicons:user-solid" color="White"/></button>
+<button><Iconify icon="lets-icons:message-alt-fill" color="White"/></button>
+<button><Iconify icon="material-symbols:settings-outline" color="White"/></button>
 </div>
 
 </header>
--- src/components/iconify/iconify.tsx
+++ src/components/iconify/iconify.tsx
@@ -3,8 +3,9 @@ interface IconifyProps {
     icon:string,
     width?:number,
     height?:number,
-    ClassName?:string
+    ClassName?:string,
+    color?:string
 }
-export const Iconify=({icon, width, height, ClassName}:IconifyProps)=>{
-    return <Icon icon={icon} width={width??24} height={height??24} className={ClassName??""} />
+export const Iconify=({icon, width, height, ClassName, color}:IconifyProps)=>{
+    return <Icon icon={icon} width={width??24} height={height??24} className={ClassName??""} color={color??""}/>
 }
