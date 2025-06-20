99f4a5ecf2841a8d18ef3d3027adfa799b5b2f99
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-06-19 22:23:02 -0300

    Colocado conteúdo teste

--- src/components/conteudoPrincipal/conteudoPrincipal.tsx
+++ src/components/conteudoPrincipal/conteudoPrincipal.tsx
@@ -1,10 +1,142 @@
 import style from "./conteudoPrincipal.module.css";
+import { Iconify } from "../iconify/iconify";
 export const ConteudoPrincipal = () => {
   return (
     <main>
       <div className={style.divPrincipal}>
         <div className={style.divInfoPerfil}>
-          <p>tchau</p>
+          <div className={style.infoPrincipalPerfil}>
+            <div className={style.fotoPerfil}>
+              <img src="./image.png" alt="Imagem usuário" />
+            </div>
+            <div className={style.infoUsuario}>
+              <div className={style.estatisticaPerfil}>
+                <div className={style.borderDiv}>
+                  <p>52</p>
+                  Scripts
+                </div>
+                <div className={style.borderDiv}>
+                  <p>24.5k</p>
+                  Seguidores
+                </div>
+                <div>
+                  <p>1386</p>
+                  Seguindo
+                </div>
+              </div>
+              <div className={style.nomePerfil}>
+                <div>
+                  <h1>Juliana Souza</h1>
+                </div>
+                <p>@juu_librarian</p>
+              </div>
+            </div>
+          </div>
+          <div className={style.tituloSobre}>
+            <h2>Sobre</h2>
+          </div>
+          <div className={style.Sobre}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            euismod volutpat fermentum. Aenean pellentesque neque at turpis
+            maximus, vel scelerisque mi porttitor. Nam vehicula eros ac suscipit
+            tempus. Duis ac mi sit amet lorem vulputate commodo ut a diam.
+            Maecenas sapien metus, tempor sed rutrum quis, feugiat vel ipsum.
+            Donec et sem convallis metus ultricies ultricies. Donec id iaculis
+            lorem. Vivamus sit amet mattis elit. Cras vel justo nec leo lacinia
+            laoreet in eget dolor. Cras nibh neque, sodales a pretium ac,
+            condimentum sed purus. Phasellus scelerisque cursus sapien sit amet
+            mattis. Morbi bibendum metus quis mauris cursus, at vulputate urna
+            ullamcorper. Donec rutrum venenatis commodo. Maecenas faucibus diam
+            in consectetur suscipit. In hac habitasse platea dictumst. Mauris
+            vitae nunc sit amet tortor euismod sagittis. Vestibulum ornare purus
+            ante, sed eleifend turpis tempus sed. Aenean vel porta risus. Mauris
+            efficitur odio ac dui porta, ac fermentum dolor pulvinar. Vestibulum
+            sed fringilla nisl. Nam et lacinia mauris. Vestibulum ante ipsum
+            primis in faucibus orci luctus et ultrices posuere cubilia curae;
+            Curabitur tincidunt elementum diam ut imperdiet. Vivamus faucibus
+            ipsum tellus. Nunc a euismod augue. Vivamus elementum, nulla vel
+            tempor sollicitudin, velit erat aliquet orci, nec tempus sapien dui
+            dictum lacus. Aliquam porta, sem at viverra fringilla, ex lectus
+            porttitor augue, quis sollicitudin justo urna at orci. Aenean
+            fermentum nisl sit amet neque pellentesque, a condimentum tellus
+            finibus. Fusce venenatis erat nec tempus elementum. Morbi mattis
+            hendrerit mi ut dapibus. Nunc volutpat mauris est, eu lobortis diam
+            venenatis at. Praesent vulputate urna id nibh sagittis finibus.
+            Curabitur scelerisque tortor ipsum, a porta orci condimentum et.
+            Praesent et metus efficitur, commodo augue eu, consequat velit.
+            Donec tristique ligula nec consectetur ullamcorper. Nullam molestie,
+            elit sit amet consequat rutrum, eros velit pulvinar felis, id
+            pulvinar urna augue quis enim. Phasellus venenatis pharetra odio,
+            sed tincidunt risus tempus vel. Phasellus massa nisi, molestie vel
+            tellus sit amet, scelerisque facilisis purus. Nam tincidunt
+            tincidunt eros, nec auctor libero lacinia non. In aliquet quis
+            libero ut tristique. Suspendisse velit libero, pretium non nulla ac,
+            suscipit luctus sapien. Nam ut nisi sollicitudin, fermentum lorem a,
+            euismod sem. Proin pellentesque feugiat libero non rhoncus. Integer
+            vestibulum leo velit, feugiat ultrices lacus rhoncus at. Sed ut
+            tortor pretium, fermentum massa sit amet, condimentum risus.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Vivamus et congue metus, sed malesuada mi.
+            Donec nec elit et lorem iaculis ullamcorper a nec metus. Donec
+            interdum, eros nec rhoncus sagittis, urna orci pharetra mi, vel
+            tristique nulla dolor et lorem. Donec vitae elit sed diam tincidunt
+            hendrerit id elementum felis. Curabitur maximus volutpat finibus.
+            Maecenas eget augue eget velit elementum commodo.
+          </div>
+          <div className={style.tituloScript}>
+            <h2>Scripts recentes</h2>
+          </div>
+          <div className={style.Scripts}>
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+                        ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+                        ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+            ddddddddddddddd
+
+          </div>
         </div>
         <div className={style.divAgora}>
           <h1>Agora...</h1>
