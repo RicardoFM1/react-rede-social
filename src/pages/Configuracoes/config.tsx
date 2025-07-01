1bc3a594a123520442f8ba2279c51d7038618cb2
Ricardo <ricardofernandesdemoura7@gmail.com>
2025-07-01 13:10:53 -0300

    Update config.tsx

--- src/pages/Configuracoes/config.tsx
+++ src/pages/Configuracoes/config.tsx
@@ -5,16 +5,16 @@ import { Link } from "react-router-dom";
 
 export const Config = () => {
   const Cores = useContext(Theme);
-  let [corTema, setCorTema] = useState(Cores);
-  const className = "Tema-" + corTema;
+  let [corTema, setCorTema] = useState(Cores); // TIRAR
+  const className = "Tema-" + corTema; // mudar para: const className = "Tema-" + Cores
   console.log(className);
   return (
     <div className={style[className]}>
       <button
         className={style.BtnTema}
         onClick={() => {
-          localStorage.setItem("Tema", corTema);
-          setCorTema(corTema === "Claro" ? "Escuro" : "Claro");
+          localStorage.setItem("Tema", corTema); // .setItem("Tema", Cores)
+          setCorTema(corTema === "Claro" ? "Escuro" : "Claro"); // TIRAR
         }}
       >
         Mudar tema
